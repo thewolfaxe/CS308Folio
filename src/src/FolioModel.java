bf846eaf65db1876c978d4723ec850d67740df0e
Ross Broomfield CES2017 <ross.broomfield.2017@uni.strath.ac.uk>
2020-03-08 11:23:26 +0000

    folio class template

--- src/src/FolioModel.java
+++ src/src/FolioModel.java
@@ -1,2 +1,46 @@
-public class FolioModel {
+public class FolioModel implements iFolioModel {
+    
+    private User user;
+    private int id;
+    private String name;
+    private ArrayList<StockModel> stocks;
+
+    public FolioModel(User user, int id, String name) {
+        stocks = new ArrayList<StockModel>();
+        this.user = user;
+        this.id = id;
+        this.name = name;
+    }
+
+    public ArrayList<StockModel> refresh() {
+        for(int i=0;i<stocks.size();i++){
+            stocks.set(i, stocks.get(i).refresh());
+        }
+        return stocks;
+    }
+
+    public ArrayList<StockModel> sort(int sortCode) {
+        //do something to sort stocks
+        return stocks;
+    }
+
+    public boolean addStock(String ticker, String name, int shares){
+        stocks.add(new StockModel(ticker, name, shares));
+        return true; //have some way of return false if ticker symbol is invalid
+    }
+
+    public boolean deleteStock(StockModel stock) {
+        stocks.remove(stock); //will probably have to override equals method in StockModel for this to work
+        return true; //false if the stock doesnt exist
+    }
+
+    public double getFolioValue() {
+        double val = 0;
+        for(int i=0;i<stocks.size();i++){
+            stocks.get(i).updateStockValue();
+            val += stocks.get(i).getValueStock();
+        }
+        return val;
+    }
+   
 }
