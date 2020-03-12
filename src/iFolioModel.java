b612043732b22112eb33d34de3a86bfb68e10ec7
Ross Broomfield CES2017 <ross.broomfield.2017@uni.strath.ac.uk>
2020-03-12 14:35:00 +0000

    updated interface to reflect folio class

--- src/iFolioModel.java
+++ src/iFolioModel.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 interface iFolioModel {
     void refreshStocks(); //calls a refresh method in each stock and returns all the updated stocks in an ArrayList
     ArrayList<StockModel> sort(int sortCode, ArrayList<StockModel> stocks);
-    boolean addStock(StockModel stock);
+    boolean addStock(String ticker, String name, int shares);
     boolean deleteStock(StockModel stock);
     double getFolioValue();
     String getName();
