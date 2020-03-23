05720bf91785b6c93c0a21c5ff9bf03c944d2e71
xeb17169 <humza.siddiq.2017@uni.strath.ac.uk>
2020-03-23 15:04:26 +0000

    SET UP table, ready to parse cell data

--- src/View/Main.java
+++ src/View/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import model.StockModel;
+import model.iStockModel;
 
 public class Main extends Application {
     @Override
@@ -30,6 +32,7 @@ public class Main extends Application {
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color:  rgba(0,0,0,0.3);");
         Button add = new Button("Add");
+        add.setOnAction((a) -> System.out.println("Hello, World!"));
         hbox.getChildren().addAll(tickerSymbol, tickerSymbol_txt, numberShares, numberShares_txt, add);
         hbox.setAlignment(Pos.CENTER);
 //            tabContent.getChildren().add(hbox);
@@ -37,26 +40,41 @@ public class Main extends Application {
         TableView<model.iStockModel> table = new TableView<>();
         table.setMinHeight(600 - hbox.getHeight());
         table.setEditable(false); //for now
-        TableColumn<model.iStockModel, String> tickerSymbolColumn = new TableColumn<>("Ticker Symbol");
+
+        TableColumn<iStockModel, String> tickerSymbolColumn = new TableColumn<>("Ticker Symbol");
         tickerSymbolColumn.setMinWidth(100);
-//        tickerSymbolColumn.setCellFactory(new PropertyValueFactory<model.iStockModel, String>("tickerSymbol"));
+        tickerSymbolColumn.setCellValueFactory(new PropertyValueFactory<>("tickerSymbol"));
+
         TableColumn<model.iStockModel, String> stockNameColumn = new TableColumn<>("Stock Name");
         stockNameColumn.setMinWidth(100);
+        stockNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+
         TableColumn<model.iStockModel, Integer> numberSharesColumn = new TableColumn<>("Number of Shares");
         numberSharesColumn.setMinWidth(100);
+        numberSharesColumn.setCellValueFactory(new PropertyValueFactory<>("noOfShares"));
+
+
         TableColumn<model.iStockModel, Double> pricePerShareColumn = new TableColumn<>("Price per Share");
         pricePerShareColumn.setMinWidth(100);
+        pricePerShareColumn.setCellValueFactory(new PropertyValueFactory<>("pricePerShare"));
+
+
         TableColumn<model.iStockModel, Double> valueOfHolding = new TableColumn<>("Value of Holding");
         valueOfHolding.setMinWidth(100);
+        valueOfHolding.setCellValueFactory(new PropertyValueFactory<>("valueOfHolding"));
 
-        table.getColumns().addAll(
-                tickerSymbolColumn,
+
+
+
+        table.getColumns().addAll(tickerSymbolColumn,
                 stockNameColumn,
                 numberSharesColumn,
                 pricePerShareColumn,
                 valueOfHolding);
         tabContent.getChildren().addAll(hbox,table);
 
+        table.getItems().add(new StockModel("h", "h",0));
 
         tab1.setContent(tabContent);
 
