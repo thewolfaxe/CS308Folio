454a197d734040d300c3730ae5be9301a200198d
lpb17179 <lpb17179@uni.strath.ac.uk>
2020-03-29 11:53:50 +0100

    wrote tests for the stock class

---
+++ src/Model/StockTests.java
@@ -0,0 +1,36 @@
+package Model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StockTests {
+    public StockModel testStockGood = new StockModel("GOOGL", "Google", 1);
+    public StockModel testStockBad = new StockModel("blah", "blah", 1);
+
+    @Test
+    public void constructTestGoodParams(){
+        Assertions.assertNotNull(testStockGood.refresh());
+    }
+
+    @Test
+    public void constructTestBadParams(){
+        Assertions.assertNull(testStockBad.refresh());
+    }
+
+    @Test
+    public void sellSharesTestGoodParams(){
+        Assertions.assertTrue(testStockGood.sellShares(1));
+    }
+
+    @Test
+    public void sellSharesTestBadParams(){
+        Assertions.assertFalse(testStockGood.sellShares(5));
+    }
+
+    @Test
+    public void buySharesTest(){
+        double firstValue = testStockGood.getValue();
+        testStockGood.buyShares(1);
+        Assertions.assertTrue(testStockGood.getValue() > firstValue);
+    }
+}
