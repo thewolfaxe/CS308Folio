2e3a4ec870da62302d7f7893d1c5d0bc758347a2
Matt Starkey <mattstar29@gmail.com>
2021-07-01 09:37:31 +0100

    cleaning up

--- To Submit/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module FolioTracker {
-    requires javafx.controls;
-    requires org.junit.jupiter.api;
-    requires javafx.fxml;
-    exports View;
-    exports Model;
-    exports Controller;
-}
